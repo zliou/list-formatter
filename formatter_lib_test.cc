396d4d036a7ccabbd3dfe50dd9dc20920e0cf0f9
Zach Liou <zachliou@gmail.com>
2023-08-24 22:46:15 -0400

    Add basic tests

--- formatter_lib_test.cc
+++ formatter_lib_test.cc
@@ -1,9 +1,32 @@
 #include "formatter_lib.cc"
 
 #include <iostream>
+#include <string>
+
+
+template<class T>
+void Test(std::string description, T expected, T actual) {
+    bool match = expected == actual;
+    std::cout << "[";
+    if (match) {
+        std::cout << " pass ";
+    } else {
+        std::cout << "FAILED";
+    }
+    std::cout << "] " << description << std::endl;
+}
+
+void RunTests() {
+    Test("DoubleQuoteTranslationSuccess",
+         Quote::DOUBLE, TranslateQuoteFromOptions(/*add_double_quotes=*/true,
+                                                  /*add_single_quotes=*/false));
+    Test("SingleQuoteTranslationSuccess",
+         Quote::SINGLE, TranslateQuoteFromOptions(/*add_double_quotes=*/false,
+                                                  /*add_single_quotes=*/true));
+}
 
 
 int main() {
-    bool match = Quote::DOUBLE == TranslateQuoteFromOptions(true, false);
-    std::cout << match << std::endl;
+    RunTests();
+    return 0;
 }
