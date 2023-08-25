5e8e5a3579651d64cd69dc75a9d493c815cd6573
Zach Liou <zachliou@gmail.com>
2023-08-24 20:52:51 -0400

    Read whether checkboxes are checked

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ var Module = {
 function formatList() {
     console.log("pressed");
     inputText = document.getElementById("input").value;
+    console.log("add-double-quotes: " + 
+            document.getElementById("add-double-quotes").checked);
+    console.log("add-single-quotes: " + 
+            document.getElementById("add-single-quotes").checked);
+    console.log("add-commas: " + 
+            document.getElementById("add-commas").checked);
     Module.PrintFormatHello(inputText);
 }
 
